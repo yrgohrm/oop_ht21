ae366b61cf1c81aa8deef665d84506af068d0f82
Hampus Ram <hampus.ram@educ.goteborg.se>
2021-09-30 14:07:25 +0200

    kommentarer

--- rep1/Methods.java
+++ rep1/Methods.java
@@ -4,14 +4,27 @@ public class Methods {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // we need to declare shouldExit outside of the loop
+        // to be able to use it in the while-condition.
         boolean shouldExit;
         do {
+            // we call handleOperation and send our scanner to it
             shouldExit = handleOperation(scanner);
         } while (!shouldExit);
     }
 
+    /**
+     * Reads a number and an operation from the given scanner and
+     * prints out the result.
+     * 
+     * @param scanner the scanner to read the data from
+     * @return true if the user wants to quit, false otherwise
+     */
     private static boolean handleOperation(Scanner scanner) {
         System.out.println("Skriv ett tal:");
+        // this will call the getInt method and pass along the scanner
+        // the result value (what's returned inside getInt) will be
+        // stored in the local variable "number"
         int number = getInt(scanner);
 
         System.out.println("Skriv in en operation dubbel, halva eller avbryt");
@@ -22,12 +35,22 @@ public class Methods {
         } else if (op.equals("halva")) {
             System.out.println(number / 2);
         } else {
+            // we have decided that we return true if the user wants to quit
             return true;
         }
 
+        // we have decided that we return false if the user wants to continue
         return false;
     }
 
+    /**
+     * Reads an integer from the given scanner and also reads and
+     * discards the rest of that line. This is to prevent any dangling
+     * newlines.
+     * 
+     * @param scanner the scanner to read from
+     * @return the integer that we read from the scanner
+     */
     private static int getInt(Scanner scanner) {
         int number = scanner.nextInt();
         scanner.nextLine();
